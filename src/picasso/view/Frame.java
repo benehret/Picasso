77a055efe116a0b90777accf7772d236f610732b
John Adekola <adekolaj24@mail.wlu.edu>
2021-12-01 14:11:40 -0500

    basic user input expressions have been created.
     Need to come together and come to terms if we need exceptions in case
    user does not input the correct expression.

--- src/picasso/view/Frame.java
+++ src/picasso/view/Frame.java
@@ -28,6 +28,7 @@ public class Frame extends JFrame {
 		commands.add("Open", new Reader());
 		commands.add("Evaluate", new ThreadedCommand<Pixmap>(canvas, new Evaluater()));
 		commands.add("Save", new Writer());
+		commands.add("Input Expression", new ThreadedCommand<Pixmap>(canvas, new EvaluaterInput()));
 
 		// add our container to Frame and show it
 		getContentPane().add(canvas, BorderLayout.CENTER);
