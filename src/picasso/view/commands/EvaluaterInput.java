ea4c341c08b4343fc3f62ca619b7348cdb7d9e92
John Adekola <adekolaj24@mail.wlu.edu>
2021-12-01 18:36:43 -0500

    More work on user input

--- src/picasso/view/commands/EvaluaterInput.java
+++ src/picasso/view/commands/EvaluaterInput.java
@@ -1,29 +1,14 @@
+
+
 package picasso.view.commands;
 
 import java.awt.Color;
-import java.util.Scanner;
-
-import javax.swing.JOptionPane;
-
 import java.awt.Dimension;
 
 import picasso.model.Pixmap;
 import picasso.parser.ExpressionTreeGenerator;
 import picasso.parser.language.ExpressionTreeNode;
 import picasso.util.Command;
-import java.awt.BorderLayout;
-import java.awt.FlowLayout;
-import java.awt.LayoutManager;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import java.util.Scanner;
-import java.awt.Dimension;
 
 
 /**
@@ -34,22 +19,26 @@ import java.awt.Dimension;
 public class EvaluaterInput implements Command<Pixmap> {
 	public static final double DOMAIN_MIN = -1;
 	public static final double DOMAIN_MAX = 1;
+	
 
 	/**
 	 * Evaluate an expression for each point in the image.
 	 */
 	public void execute(Pixmap target) {
-		  TextBox happy= new TextBox();
-		 String result= happy.getResult();
-		ExpressionTreeNode expr = createExpression(result);
-		// evaluate it for each pixel
-		Dimension size = target.getSize();
-		for (int imageY = 0; imageY < size.height; imageY++) {
-			double evalY = imageToDomainScale(imageY, size.height);
-			for (int imageX = 0; imageX < size.width; imageX++) {
-				double evalX = imageToDomainScale(imageX, size.width);
-				Color pixelColor = expr.evaluate(evalX, evalY).toJavaColor();
-				target.setColor(imageX, imageY, pixelColor);
+		TextBox happy= new TextBox();
+		happy.frame();
+		if (happy.getAnswer() == true) {
+			String result = happy.getResult();
+			ExpressionTreeNode expr = createExpression(result);
+			// evaluate it for each pixel
+			Dimension size = target.getSize();
+			for (int imageY = 0; imageY < size.height; imageY++) {
+				double evalY = imageToDomainScale(imageY, size.height);
+				for (int imageX = 0; imageX < size.width; imageX++) {
+					double evalX = imageToDomainScale(imageX, size.width);
+					Color pixelColor = expr.evaluate(evalX, evalY).toJavaColor();
+					target.setColor(imageX, imageY, pixelColor);
+				}
 			}
 		}
 	}
