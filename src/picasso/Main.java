60c6c2bf188c6d59cfe4c4546a2bc9a76e8c167c
John Adekola <adekolaj24@mail.wlu.edu>
2021-12-02 15:54:55 -0500

    more changes fixing

--- src/picasso/Main.java
+++ src/picasso/Main.java
@@ -11,7 +11,6 @@ import picasso.view.Frame;
  */
 public class Main {
 	public static final Dimension SIZE = new Dimension(600, 600);
-	public static Frame frame;
 
 	public static void main(String[] args) {
 		Frame frame = new Frame(SIZE);
